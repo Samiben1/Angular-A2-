cc0f45b6b715a04bc8a29ff83a59d729b7c5aff1
Samiben1 <samiben498@gmail.com>
2019-09-23 23:43:50 +1000

    frontend components

--- chat/src/app/app.module.ts
+++ chat/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    LoginComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
