6c67862d272641d42d15f99926e979ddd3919b2d
Samiben1 <samiben498@gmail.com>
2019-09-30 13:40:23 +1000

    creation of api files

--- chat/server/server.js
+++ chat/server/server.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var path = require('path');
+// var path = require('path');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-
+const io = require('socket.io')(http);
+//const socket = require('./socket.js')
 
 var Port = 3000;
+var dbname = 'mydb';
+
 var MongoClient = require('mongodb').MongoClient;
+var objectID = require('mongodb').ObjectID;
 var dburl = 'mongodb://localhost:27017/';
 
 app.use(cors());
@@ -20,8 +24,8 @@ MongoClient.connect(dburl, { useNewUrlParser: true,useUnifiedTopology: true }, f
         console.log("DB connection failed");
         return;
     }
-    var dbo = db.db('mydb');
-    dbo.collection('users').find({}).toArray(function(err, result){
+    var dbo = db.db(dbname);
+    dbo.collection('products').find({}).toArray(function(err, result){
         if (err) {
             console.log("no such collection");
             return;
@@ -29,7 +33,18 @@ MongoClient.connect(dburl, { useNewUrlParser: true,useUnifiedTopology: true }, f
         console.log(result);
         db.close();
     });
-    //console.log("DB connecteion open", db);
+    console.log("DB connecteion open");
+    require('./api/create_user.js');
+    require('./api/create_group.js');
+    require('./api/create_channel.js');
+    require('./api/add_user_to_group.js');
+    require('./api/add_user_to_channel.js');
+    require('./api/delete_user.js');
+    require('./api/delete_group.js');
+    require('./api/delete_channel.js');
+    require('./api/remove_user_from_group.js');
+    require('./api/remove_user_from_channel.js');
+
 });
 
 app.get('/', function(req, res){
@@ -39,7 +54,6 @@ app.get('/', function(req, res){
 
 
 
-var listen = app.listen(Port,function(){
+var listen = http.listen(Port,function(){
     console.log("Server started at port:", Port);
-
 });
