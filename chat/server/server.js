72823e7466256866a5ef655e5cfaabb417b1eed8
Samiben1 <samiben498@gmail.com>
2019-09-30 11:16:19 +1000

    nodemon and api

--- chat/server/server.js
+++ chat/server/server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var http = require('http').Server(app);
 var bodyParser = require('body-parser');
 var Port = 3000;
-
-app.listen(Port);
-console.log("Server started at port:", Port);
+app.get('/', function(req, res){
+    console.log("Get the home page");
+    res.send("Home page");
+});
+var listen = app.listen(Port,function(){
+    console.log("Server started at port:", Port);
+});
